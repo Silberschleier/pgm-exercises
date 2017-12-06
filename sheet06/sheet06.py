eefa43616815e28886935e6c9759b95cee16319f
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-12-06 22:36:57 +0100

    Versuch :D

--- sheet06/sheet06.py
+++ sheet06/sheet06.py
@@ -10,37 +10,40 @@ import h5py
 import numpy as np
 from pprint import pprint
 from itertools import product
+from copy import deepcopy
+
 
 def read_data():
     # read pot data
     pot_table = []
     with h5py.File("pot.mat") as f:
         for column in f['pot_table']:
-            for row_number in range(len(column)):             
+            for row_number in range(len(column)):
                 pot_table.append(f[column[row_number]][:])
-    
+
     #pprint(np.transpose(pot_table))
-    
-    
+
+
     # read pot_variables data
     pot_variables = []
     with h5py.File("pot.mat") as f:
         for column in f['pot_variables']:
-            for row_number in range(len(column)):            
+            for row_number in range(len(column)):
                 pot_variables.append(f[column[row_number]][:])
-                
+
     #pprint(np.transpose(pot_variables))
-    
+
     # fill a list with variable names
     variables = []
     for i in range(1,21):
         variables.append('d'+str(i))
     for i in range(1,41):
         variables.append('s'+str(i))
-    
+
     #pprint(variables)
-    
+
     return pot_table, pot_variables, variables
+
 
 def moralize(pot_variables):
     new_pot_variables = pot_variables
@@ -51,10 +54,10 @@ def moralize(pot_variables):
                     new_pot_variables[int(pot_var_1[0])-1] = np.append(new_pot_variables[int(pot_var_1[0])-1], pot_var_2)
     return new_pot_variables
 
-  
+
 def clique_graph(pot_variables):
     cliques = {}
-    sepsets = []
+    sepsets = {}
     # find all cliques
     ind = 1
     for variable1 in range(len(pot_variables)):
@@ -76,8 +79,9 @@ def clique_graph(pot_variables):
         for key2 in cliques:
             if key1 != key2:
                 if np.intersect1d(cliques[key1], cliques[key2]).size != 0:
-                    sepsets.append(str(key1) + (', ') + str(key2) + ': ' + str(np.intersect1d(cliques[key1], cliques[key2])))
+                    sepsets[(key1, key2)] = np.intersect1d(cliques[key1], cliques[key2])
     return { 'cliques': cliques, 'sepsets': sepsets }
+
 
 def clique_potentials(cliques, pot_table, pot_variables):
     clique_potentials = {}
@@ -85,12 +89,54 @@ def clique_potentials(cliques, pot_table, pot_variables):
         prod = 1
         for var in cliques[key]:
             prod = np.multiply(prod, pot_table[var-1])
-        clique_potentials.update({key: prod})
+        clique_potentials[key] = prod
     return clique_potentials
 
+
 def pass_messages(cliques, sepsets, clique_potentials):
-    updated_clique_potentials = 1
+    updated_clique_potentials = deepcopy(clique_potentials)
+    downward_message = pass_downward('1', set(), cliques, sepsets, updated_clique_potentials)
+    pass_upward('1', downward_message, set(), cliques, sepsets, updated_clique_potentials)
     return updated_clique_potentials
+
+
+def get_neighbours(clique_id, sepsets):
+    neighbours = []
+    for key, value in sepsets.items():
+        if key[0] == clique_id:
+            neighbours.append(key[1])
+        elif key[1] == clique_id:
+            neighbours.append(key[0])
+
+    return neighbours
+
+
+def pass_downward(clique_id, visited, cliques, sepsets, clique_potentials):
+    neighbours = get_neighbours(clique_id, sepsets)
+    message = clique_potentials[clique_id]
+    visited.add(clique_id)
+    for neighbour_id in neighbours:
+        if neighbour_id in visited:
+            continue
+        temp_msg = pass_downward(neighbour_id, visited, cliques, sepsets, clique_potentials)
+        message = np.multiply(message, temp_msg)
+
+    clique_potentials[clique_id] = message
+
+    return message
+
+
+def pass_upward(clique_id, message, visited, cliques, sepsets, clique_potentials):
+    temp_message = np.multiply(clique_potentials[clique_id], message)
+    clique_potentials[clique_id] = temp_message
+
+    neighbours = get_neighbours(clique_id, sepsets)
+    visited.add(clique_id)
+    for neighbour_id in neighbours:
+        if neighbour_id in visited:
+            continue
+        pass_upward(neighbour_id, temp_message, visited, cliques, sepsets, clique_potentials)
+
 
 def compute_marginal(var, cliques, clique_potentials):
     for key in cliques:
@@ -114,6 +160,6 @@ updated_clique_potentials = pass_messages(junction_tree['cliques'], junction_tre
 
 #return marginals
 for var in range(20,60):
-    pprint("Marginal for " + str(variables[var]) + ": " + str(compute_marginal(var, junction_tree['cliques'], clique_potentials)))
+    print("Marginal for " + str(variables[var]) + ": " + str(compute_marginal(var, junction_tree['cliques'], clique_potentials)))
 
 
