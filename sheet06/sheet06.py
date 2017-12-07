67159d40dab1323656b46f58353f873dbbd3abf0
aikira7 <aikira7@web.de>
2017-12-07 10:57:08 +0100

    Implementierung und Ausgabe angepasst

--- sheet06/sheet06.py
+++ sheet06/sheet06.py
@@ -21,9 +21,6 @@ def read_data():
             for row_number in range(len(column)):
                 pot_table.append(f[column[row_number]][:])
 
-    #pprint(np.transpose(pot_table))
-
-
     # read pot_variables data
     pot_variables = []
     with h5py.File("pot.mat") as f:
@@ -31,16 +28,12 @@ def read_data():
             for row_number in range(len(column)):
                 pot_variables.append(f[column[row_number]][:])
 
-    #pprint(np.transpose(pot_variables))
-
     # fill a list with variable names
     variables = []
     for i in range(1,21):
         variables.append('d'+str(i))
     for i in range(1,41):
         variables.append('s'+str(i))
-
-    #pprint(variables)
 
     return pot_table, pot_variables, variables
 
@@ -93,10 +86,10 @@ def clique_potentials(cliques, pot_table, pot_variables):
     return clique_potentials
 
 
-def pass_messages(cliques, sepsets, clique_potentials):
+def pass_messages(cliques, sepsets, clique_potentials, start_id):
     updated_clique_potentials = deepcopy(clique_potentials)
-    downward_message = pass_downward('1', set(), cliques, sepsets, updated_clique_potentials)
-    pass_upward('1', downward_message, set(), cliques, sepsets, updated_clique_potentials)
+    downward_message = pass_downward(start_id, set(), cliques, sepsets, updated_clique_potentials)
+    pass_upward(start_id, downward_message, set(), cliques, sepsets, updated_clique_potentials)
     return updated_clique_potentials
 
 
@@ -112,36 +105,40 @@ def get_neighbours(clique_id, sepsets):
 
 
 def pass_downward(clique_id, visited, cliques, sepsets, clique_potentials):
+    sum_pot = np.sum(clique_potentials[clique_id], axis=1)   
     neighbours = get_neighbours(clique_id, sepsets)
     message = clique_potentials[clique_id]
     visited.add(clique_id)
     for neighbour_id in neighbours:
         if neighbour_id in visited:
             continue
-        temp_msg = pass_downward(neighbour_id, visited, cliques, sepsets, clique_potentials)
+        temp_msg = sum_pot * pass_downward(neighbour_id, visited, cliques, sepsets, clique_potentials)
         message = np.multiply(message, temp_msg)
-
     clique_potentials[clique_id] = message
 
     return message
 
 
 def pass_upward(clique_id, message, visited, cliques, sepsets, clique_potentials):
-    temp_message = np.multiply(clique_potentials[clique_id], message)
-    clique_potentials[clique_id] = temp_message
-
+    sum_pot = np.sum(clique_potentials[clique_id], axis=1)   
     neighbours = get_neighbours(clique_id, sepsets)
     visited.add(clique_id)
     for neighbour_id in neighbours:
         if neighbour_id in visited:
             continue
+        temp_message = np.multiply(message, sum_pot)
         pass_upward(neighbour_id, temp_message, visited, cliques, sepsets, clique_potentials)
+        clique_potentials[clique_id] = temp_message
 
+def get_clique_id(var, cliques):
+    for key in cliques:
+        if var in cliques[key]:
+            return key
 
 def compute_marginal(var, cliques, clique_potentials):
     for key in cliques:
         if var in cliques[key]:
-            return np.sum(np.sum(clique_potentials[key], axis=(0,1)), axis=0)
+            return np.sum(np.sum(clique_potentials[key], axis=(0,1)), axis=1)
 
 
 pot_table, pot_variables, variables = read_data()
@@ -153,13 +150,35 @@ new_pot_variables_after_moralization = moralize(pot_variables)
 junction_tree = clique_graph(new_pot_variables_after_moralization)
 
 # assign potentials to the cliques
-clique_potentials = clique_potentials(junction_tree['cliques'], pot_table, pot_variables)
+old_clique_potentials = clique_potentials(junction_tree['cliques'], pot_table, pot_variables)
 
-# pass messages and update clique potentials
-updated_clique_potentials = pass_messages(junction_tree['cliques'], junction_tree['sepsets'], clique_potentials)
-
+print("Marginals for the symptoms:\n")
 #return marginals
 for var in range(20,60):
-    print("Marginal for " + str(variables[var]) + ": " + str(compute_marginal(var, junction_tree['cliques'], clique_potentials)))
+    # pass messages and update clique potentials
+    updated_clique_potentials = pass_messages(junction_tree['cliques'], junction_tree['sepsets'], old_clique_potentials, get_clique_id(var+1, junction_tree['cliques']))
+    margs = compute_marginal(var+1, junction_tree['cliques'], updated_clique_potentials)
+    res1 = margs[0] / (margs[0] + margs[1])
+    res2 = margs[1] / (margs[0] + margs[1])
+    print("Marginal for " + str(variables[var]) + ": " + str(res1) + " | " + str(res2))
+
+# dealing with evidence
+new_pot_table = deepcopy(pot_table)
+for var in range(20,25):
+    new_pot_table[var] = new_pot_table[var][0]
+for var in range(26,30):
+    new_pot_table[var] = new_pot_table[var][1]
 
 
+# assign new potentials to the cliques
+new_clique_potentials = clique_potentials(junction_tree['cliques'], new_pot_table, pot_variables)
+
+print("\n\nMarginals for the diseases, given s1-5 are present and s6-10 are not:\n")
+#return marginals for d=1
+for var in range(0,20):
+    # pass messages and update clique potentials
+    updated_clique_potentials = pass_messages(junction_tree['cliques'], junction_tree['sepsets'], new_clique_potentials, get_clique_id(var+1, junction_tree['cliques']))
+    margs = compute_marginal(var+1, junction_tree['cliques'], updated_clique_potentials)
+    res1 = margs[0] / (margs[0] + margs[1])
+    res2 = margs[1] / (margs[0] + margs[1])
+    print("Marginal for " + str(variables[var]) + ": " + str(res1) + " | " + str(res2))
